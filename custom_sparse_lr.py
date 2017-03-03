539cbcd10955f5540484bef0054358195971c1fe
tonytongzhao <tony.tong.zhao@gmail.com>
2017-03-03 21:12:17 +0800

    sparse feature selection added

--- custom_sparse_lr.py
+++ custom_sparse_lr.py
@@ -2,13 +2,12 @@ import mxnet as mx
 import numpy as np
 import sys
 import os
-
+from multi_devices_mlp import ToyData
 class SparseLinear(mx.operator.CustomOp):
     def __init__(self, is_data, sparse_reg):
         self.sparse_reg=float(sparse_reg)
         #self.weight=weight
         self.is_data=int(is_data)
-        self.dim=dim
 
     def forward(self, is_train, req, in_data, out_data, aux):
         self.assign(out_data[0], req[0], mx.nd.broadcast_mul(in_data[0], in_data[1]))
@@ -18,7 +17,7 @@ class SparseLinear(mx.operator.CustomOp):
             in_grad[0][:]=mx.nd.zeros(in_data[0].shape)
         else:
             in_grad[0][:]=(in_data[1]*mx.nd.sum_axis(out_grad[0],axis=0)).broadcast_to(in_data[0].shape)
-        in_grad[1][:]=mx.nd.sum_axis(in_data[0], axis=0)*mx.nd.sum_axis(out_grad[0],axis=0)/in_data[0].shape[0]+((in_data[1]>0)*2-1)*self.sparse_reg
+        in_grad[1][:]=mx.nd.sum_axis(in_data[0], axis=0)*mx.nd.sum_axis(out_grad[0],axis=0)+((in_data[1]>0)*2-1)*self.sparse_reg
 @mx.operator.register('sparse_linear')
 
 class SparseLinearProp(mx.operator.CustomOpProp):
@@ -46,34 +45,51 @@ class SparseLinearProp(mx.operator.CustomOpProp):
 
 if __name__=='__main__':
     if True:    
-        m,n=2,5
-        pos,neg=1.0, 0.1
-        dim=(m*2,n)
+        num_class=10
+        num_feature=50
+        batch_size=100
+        toy_data=ToyData(num_class, num_feature)
+        
         data=mx.sym.Variable('data')
         sl_weight=mx.sym.Variable('sl_weight')
-        sldata=mx.sym.Custom(data=data, weight=sl_weight, is_data=1, sparse_reg=0.01, name='sl', op_type='sparse_linear')
-        slr=mx.sym.FullyConnected(data=sldata, num_hidden=15, name='fc1')
-        #data=mx.sym.FullyConnected(data=data, num_hidden=5)
-        #wlr=mx.sym.Custom(data=slr, pos_grad_scale=pos, neg_grad_scale=neg, name='wlr', op_type='weighted_logistic_regression')
-        slr=mx.sym.sum_axis(data=slr, axis=1, name='sum1')
-        slr=mx.sym.Flatten(data=slr, name='flatten1')
-        wlr=mx.sym.LinearRegressionOutput(data=slr, name='wlr')
+        data=mx.sym.Custom(data=data, weight=sl_weight, is_data=1, sparse_reg=0.01, name='sl', op_type='sparse_linear')
 
-        input_shape={'data':(2*m,n), 'sl_weight': (1,n), 'wlr_label':(2*m,1)}
+        #first full c
+        fc1=mx.sym.FullyConnected(data=data,num_hidden=500)
+        tanh3=mx.sym.Activation(fc1, act_type='tanh')
+
+        #second full c
+        fc2=mx.sym.FullyConnected(tanh3, num_hidden=num_class)
+    
+        #wlr=mx.sym.Custom(data=slr, pos_grad_scale=pos, neg_grad_scale=neg, name='wlr', op_type='weighted_logistic_regression')
+        wlr=mx.sym.SoftmaxOutput(data=fc2, name='wlr')
+
+        input_shape={'data':(batch_size, num_feature),  'wlr_label':(batch_size,),'sl_weight': (1,num_feature)}
+
         executor=wlr.simple_bind(ctx=mx.gpu(), grad_req='write',**input_shape )
 
+        args=dict(zip(wlr.list_arguments(), executor.arg_arrays))
         for r in executor.arg_arrays:
             r[:]=np.random.randn(*r.shape)*0.02
+        for epoch in xrange(1000):
+            x,y=toy_data.get(batch_size)
+            executor.arg_dict['data'][:]=mx.nd.array(x,ctx=mx.gpu())
+            executor.arg_dict['wlr_label'][:]=mx.nd.array(y,ctx=mx.gpu()).reshape(args['wlr_label'].shape)
+            #executor.arg_dict['sl_weight'][:]=mx.nd.array(np.random.rand(1,num_feature),ctx=mx.gpu())
 
-        executor.arg_dict['data'][:]=mx.nd.array(np.arange(20).reshape((4,5)),ctx=mx.gpu())
-        executor.arg_dict['wlr_label'][:]=mx.nd.array(np.vstack([np.ones((m,1)), np.zeros((m,1))]), ctx=mx.gpu())
-        executor.arg_dict['sl_weight'][:]=mx.nd.array(np.random.rand(1,n),ctx=mx.gpu())
+            executor.forward(is_train=True)
+            executor.backward()    
+            #print 'arguments', wlr.list_arguments()
+            #print 'arg_arrays', [x for x in executor.arg_arrays]
+            #print 'grad_arrays', [x.asnumpy() for x in executor.grad_arrays]
+            for pname, w, grad in zip(wlr.list_arguments(), executor.arg_arrays, executor.grad_arrays):
+                if pname in ['data', 'wlr_label']:
+                    continue
+                w[:]-=grad*0.05/batch_size
 
-        executor.forward()
-        executor.backward()    
+            if epoch%100==0:
+                pred=mx.nd.argmax_channel(executor.outputs[0]).asnumpy()
+                print 'epoch %d, acc %f'%(epoch, (pred==y.flatten()).sum()/(batch_size+0.0))
+                print 'feature selection', args['sl_weight'].asnumpy()
 
-        print 'arguments', wlr.list_arguments()
-        #print 'arg_arrays', [x for x in executor.arg_arrays]
-        print 'grad_arrays', [x.asnumpy() for x in executor.grad_arrays]
-        
         
