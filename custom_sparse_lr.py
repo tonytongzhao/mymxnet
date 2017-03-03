6b274bc452bc9a12414b3ef243f7d40292f12c3b
tonytongzhao <tony.tong.zhao@gmail.com>
2017-03-03 17:26:54 +0800

    sparse linear op finished

--- custom_sparse_lr.py
+++ custom_sparse_lr.py
@@ -15,10 +15,8 @@ class SparseLinear(mx.operator.CustomOp):
 
     def backward(self, req, out_grad, in_data, out_data, in_grad, aux):
         if self.is_data:
-            print 'in_grad[0] is zero'
             in_grad[0][:]=mx.nd.zeros(in_data[0].shape)
         else:
-            print 'in_grad[0] will be assigned here'
             in_grad[0][:]=(in_data[1]*mx.nd.sum_axis(out_grad[0],axis=0)).broadcast_to(in_data[0].shape)
         in_grad[1][:]=mx.nd.sum_axis(in_data[0], axis=0)*mx.nd.sum_axis(out_grad[0],axis=0)/in_data[0].shape[0]+((in_data[1]>0)*2-1)*self.sparse_reg
 @mx.operator.register('sparse_linear')
@@ -53,7 +51,7 @@ if __name__=='__main__':
         dim=(m*2,n)
         data=mx.sym.Variable('data')
         sl_weight=mx.sym.Variable('sl_weight')
-        sldata=mx.sym.Custom(data=data, weight=sl_weight, is_data=0, sparse_reg=0.01, name='sl', op_type='sparse_linear')
+        sldata=mx.sym.Custom(data=data, weight=sl_weight, is_data=1, sparse_reg=0.01, name='sl', op_type='sparse_linear')
         slr=mx.sym.FullyConnected(data=sldata, num_hidden=15, name='fc1')
         #data=mx.sym.FullyConnected(data=data, num_hidden=5)
         #wlr=mx.sym.Custom(data=slr, pos_grad_scale=pos, neg_grad_scale=neg, name='wlr', op_type='weighted_logistic_regression')
