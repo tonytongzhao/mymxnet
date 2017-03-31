521a956da729153acdd480b3aead677bea07af4c
tonytongzhao <tony.tong.zhao@gmail.com>
2017-03-31 20:48:25 +0800

    batchnorm and sigmoid act updated

--- cdmemnn/run.py
+++ cdmemnn/run.py
@@ -1,14 +1,14 @@
 import os
 
 if __name__=='__main__':
-    eta=[0.001]
-    upass=[5,10,15,20]
-    ipass=[5,10,15,20]
-    num_embed=[100]
-    num_hidden=[100]
+    eta=[0.001, 0.01]
+    upass=[2,5,10]
+    ipass=[2,5,10]
+    num_embed=[10,30,50,100]
+    num_hidden=[10,30,50,100]
     for e in eta:
         for u in upass:
             for i in ipass:
                 for embed in num_embed:
                     for hidden in num_hidden:
-                        os.system('python cdmemnn.py -train ~/Data/ml-100k/u.data -eta %f -upass %d -ipass %d -nembed %d -nhidden %d &' %(e,u,i,embed,hidden))
+                        os.system('python cdmemnn.py -train ~/Data/ml-1m/ratings.dat.tr -val ~/Data/ml-1m/ratings.dat.te -eta %f -upass %d -ipass %d -nembed %d -nhidden %d &' %(e,u,i,embed,hidden))
