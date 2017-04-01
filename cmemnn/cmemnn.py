ae6167d4a64019ffd45b1d66acf7ec807c392ee2
tonytongzhao <tony.tong.zhao@gmail.com>
2017-04-01 22:41:55 +0800

    split data added

--- cmemnn/cmemnn.py
+++ cmemnn/cmemnn.py
@@ -98,7 +98,7 @@ def train(args,data_path, data, val, split, network, batch_size, num_epoch, user
     #logging.basicConfig(filename=os.path.join('/'.join(data_path.split('/')[:-1]+['res']), logname+'.log'), level=logging.DEBUG)
     logging.basicConfig(level=logging.DEBUG)
     logging.info('start with arguments %s', args)
-    network.fit(train, eval_data=val_iter, eval_metric=RMSE, optimizer_params={'learning_rate':learning_rate, 'momentum':0.9}, num_epoch=num_epoch, batch_end_callback=mx.callback.Speedometer(batch_size, 500))
+    network.fit(train, eval_data=val_iter, eval_metric=RMSE, optimizer_params={'learning_rate':learning_rate, 'momentum':0.9}, num_epoch=num_epoch, batch_end_callback=mx.callback.Speedometer(batch_size, 3000))
     '''
     for i in xrange(num_epoch):
         batch_data=random.sample(data, batch_size)
---
+++ rec_sys_random_split.py
@@ -0,0 +1,36 @@
+import sys
+import os
+import collections
+import random
+dfile=sys.argv[1]
+path='/'.join(dfile.split('/')[:-1])
+data=[]#collections.defaultdict(dict)
+nins=0
+with open(dfile, 'r') as f:
+    for line in f:
+        tks=line.split('::')
+        user,item, rate=tks[0], tks[1], tks[2]
+        data.append([user, item, rate]) 
+        nins+=1
+print nins
+tr_ins=random.sample(range(nins), int(0.9*nins))
+te_ins=list(set(range(nins))-set(tr_ins))
+print len(tr_ins)
+print len(te_ins)
+dftr=path+'/'+dfile.split('/')[-1]+'.random.tr'
+dfte=path+'/'+dfile.split('/')[-1]+'.random.te'
+wtr=0
+wte=0
+with open(dftr, 'w') as tfw:
+    for i in tr_ins:        
+        u,i,r=data[i]
+        tfw.write('%s\t%s\t%s\n' %(u, i, r))
+        wtr+=1
+tfw.close()
+with open(dfte, 'w') as tfw:
+    for i in te_ins:        
+        u,i,r=data[i]
+        tfw.write('%s\t%s\t%s\n' %(u, i, r))
+        wtr+=1
+
+tfw.close()
